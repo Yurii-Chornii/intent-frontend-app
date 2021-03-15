c40b9b27f1dd441d96007919d632130a9eee1c18
mikiyur <mikiyur@gmail.com>
2021-03-15 20:31:50 +0200

    Page tour details was added

--- tour-agency/src/components/Tour/Tour.tsx
+++ tour-agency/src/components/Tour/Tour.tsx
@@ -2,6 +2,8 @@ import {Link, useParams} from "react-router-dom";
 import data from "../../store/Data"
 import {observer} from "mobx-react-lite";
 import {useEffect} from "react";
+import "./Tour.scss"
+import {log} from "util";
 
 
 interface IParams {
@@ -12,16 +14,42 @@ const Tour = observer(() => {
     const params = useParams<IParams>();
     useEffect(() => {
         data.findAndSetCurrentTour(+params.id);
-    }, [params])
 
+        return () => {
+            data.setCurrentTour(undefined);
+        }
+    }, [params]);
 
     return (
         <div>
             <Link to="/tours">
-                <button>back to tours list</button>
+                <button className="tour-page-button">back to list</button>
             </Link>
-            {data.currentTour ? data.currentTour.title : "tour in not found"}
+            {
+                data.currentTour ? (
+                    <div className="tour">
+                        <header className="tour__header">
+                            <img src={data.currentTour.imageUrl} alt={data.currentTour.title}/>
+                        </header>
+                        <section>
+                            <article>
+                                <h3 className="tour__title">{data.currentTour.title}</h3>
+                                <p className="tour__description">
+                                    {data.currentTour.description}
+                                </p>
+                            </article>
+                        </section>
+                        <footer className="tour__footer">
+                            <div>
+                                {true && <button className="tour-page-button">add to cart</button>}
+                            </div>
+                            <b>{data.currentTour.price}</b>
+                        </footer>
+                    </div>) : (<div>
+                    <h2>"tour is not found"</h2>
+                </div>)
+            }
         </div>
     );
-})
+});
 export default Tour;
